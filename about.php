4e1b0d067f9c3c1256ecbbef2c9c61d18737adf4
MKhansa067 <105094399+MKhansa067@users.noreply.github.com>
2024-10-11 22:20:58 +0700

    Update about.php
    untuk page "About"

--- about.php
+++ about.php
@@ -2,25 +2,25 @@
 
 <div class="container mt-5">
     <h1>About Me</h1>
-    <p>Hello! My name is Muhamad Khansa Khalifaturohman. I am a Rookie Programmer. This page gives a brief description about me, my journey, and my expertise.</p>
+    <p>Hello! My name is [your name]. I am a Rookie Programmer. This page gives a brief description about me, my journey, and my expertise.</p>
     <p>I have a passion for making websites, and I love working on projects related to PHP Website Development.</p>
     <p>Feel free to connect with me on social media:</p>
 
     <!-- Ikon-ikon kontak -->
     <div class="social-icons">
-        <a href="https://wa.me/6282258135884" target="_blank" class="me-3">
+        <a href="https://wa.me/6212345678910" target="_blank" class="me-3">
             <i class="fab fa-whatsapp fa-2x"></i>
         </a>
-        <a href="https://www.instagram.com/mkhnsa675" target="_blank" class="me-3">
+        <a href="https://www.instagram.com/username" target="_blank" class="me-3">
             <i class="fab fa-instagram fa-2x"></i>
         </a>
-        <a href="https://www.facebook.com/muhammad.khansa.7359" target="_blank" class="me-3">
+        <a href="https://www.facebook.com/username" target="_blank" class="me-3">
             <i class="fab fa-facebook fa-2x"></i>
         </a>
-        <a href="https://twitter.com/kelvin_amigos" target="_blank" class="me-3">
+        <a href="https://twitter.com/username" target="_blank" class="me-3">
             <i class="fab fa-twitter fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/Muhamad Khansa Khalifaturohman" target="_blank" class="me-3">
+        <a href="https://www.linkedin.com/in/username" target="_blank" class="me-3">
             <i class="fab fa-linkedin fa-2x"></i>
         </a>
     </div>
